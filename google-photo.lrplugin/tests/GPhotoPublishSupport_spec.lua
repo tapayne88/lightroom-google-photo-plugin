97b812ac313681e7a95f2645610c1b19d08f29be
Tom Payne <tom@tompayne.dev>
2023-02-26 22:47:42 +0000

    Expand GPhotoPublishSupport tests

--- google-photo.lrplugin/tests/GPhotoPublishSupport_spec.lua
+++ google-photo.lrplugin/tests/GPhotoPublishSupport_spec.lua
@@ -37,4 +37,47 @@ describe("GPhotoPublishSupport", function()
 			}, collectionBehaviorInfo)
 		end)
 	end)
+
+	describe("deletePhotosFromPublishedCollection", function()
+		describe("when passed an empty list of photo IDs", function()
+			it("should not call the callback", function()
+				local deleteCallbackSpy = spy.new(function() end)
+
+				---@diagnostic disable-next-line: undefined-global
+				GPhotoPublishSupport.deletePhotosFromPublishedCollection(nil, {}, deleteCallbackSpy)
+
+				assert.spy(deleteCallbackSpy).was.not_called()
+			end)
+		end)
+
+		describe("when passed a number of photo IDs", function()
+			it("should pass each photoId to the callback", function()
+				local deleteCallbackSpy = spy.new(function() end)
+
+				---@diagnostic disable-next-line: undefined-global
+				GPhotoPublishSupport.deletePhotosFromPublishedCollection(nil, { 4, 5, 6 }, deleteCallbackSpy)
+
+				assert.spy(deleteCallbackSpy).was.called(3)
+				assert.spy(deleteCallbackSpy).was.called_with(4)
+				assert.spy(deleteCallbackSpy).was.called_with(5)
+				assert.spy(deleteCallbackSpy).was.called_with(6)
+			end)
+		end)
+	end)
+
+	describe("metadataThatTriggersRepublish", function()
+		it("should return a table with the correct keys", function()
+			---@diagnostic disable-next-line: undefined-global
+			local metadata = GPhotoPublishSupport.metadataThatTriggersRepublish()
+
+			assert.are.same({
+				default = false,
+				title = false,
+				caption = false,
+				keywords = false,
+				gps = false,
+				dateCreated = false,
+			}, metadata)
+		end)
+	end)
 end)
