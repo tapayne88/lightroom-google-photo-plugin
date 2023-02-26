f28649964ab529a71d79d44904964dcf00abeff5
Tom Payne <tom@tompayne.dev>
2023-02-26 13:21:31 +0000

    Expanded tests of URLHandler

--- google-photo.lrplugin/tests/GPhotoURLHandler_spec.lua
+++ google-photo.lrplugin/tests/GPhotoURLHandler_spec.lua
@@ -18,16 +18,74 @@ describe("GPhotoURLHandler", function()
 	end)
 
 	describe("URLHandler", function()
-		describe("called with a URL containing a code and GPhotoAPI.URLCallback is set", function()
-			before_each(function()
-				GPhotoAPIMock.URLCallback = spy.new(function() end)
+		local url_with_code = "lightroom://org.tapayne88.lightroom-google-photo-plugin#?code=abc123"
+		local url_without_code = "lightroom://org.tapayne88.lightroom-google-photo-plugin"
+		local non_lr_url = ""
+
+		describe("when GPhotoAPI.URLCallback is set", function()
+			describe("and the handler is called with a URL containing a code", function()
+				before_each(function()
+					GPhotoAPIMock.URLCallback = spy.new(function() end)
+				end)
+
+				it("should call GPhotoAPI.URLCallback with the URLs code param", function()
+					GPhotoURLHandler.URLHandler(url_with_code)
+
+					assert.spy(GPhotoAPIMock.URLCallback).was.called()
+					assert.spy(GPhotoAPIMock.URLCallback).was.called_with("abc123")
+				end)
 			end)
 
-			it("calls GPhotoAPI.URLCallback with the URLs code param", function()
-				GPhotoURLHandler.URLHandler("lightroom://org.tapayne88.lightroom-google-photo-plugin#?code=abc123")
+			describe("and the handler is called with a URL not containing a code", function()
+				before_each(function()
+					GPhotoAPIMock.URLCallback = spy.new(function() end)
+				end)
 
-				assert.spy(GPhotoAPIMock.URLCallback).was.called()
-				assert.spy(GPhotoAPIMock.URLCallback).was.called_with("abc123")
+				it("should call GPhotoAPI.URLCallback with nil", function()
+					GPhotoURLHandler.URLHandler(url_without_code)
+
+					assert.spy(GPhotoAPIMock.URLCallback).was.called()
+					assert.spy(GPhotoAPIMock.URLCallback).was.called_with(nil)
+				end)
+			end)
+
+			describe("and the handler is called with a non lightroom URL", function()
+				before_each(function()
+					GPhotoAPIMock.URLCallback = spy.new(function() end)
+				end)
+
+				it("should call GPhotoAPI.URLCallback with nil", function()
+					GPhotoURLHandler.URLHandler(non_lr_url)
+
+					assert.spy(GPhotoAPIMock.URLCallback).was.called()
+					assert.spy(GPhotoAPIMock.URLCallback).was.called_with(nil)
+				end)
+			end)
+		end)
+
+		describe("and GPhotoAPI.URLCallback is not set", function()
+			describe("and the handler is called with a URL containing a code", function()
+				before_each(function()
+					GPhotoAPIMock.URLCallback = nil
+				end)
+
+				it("shouldn't call GPhotoAPI.URLCallback or error", function()
+					assert.has_no.errors(function()
+						GPhotoURLHandler.URLHandler(url_with_code)
+					end)
+				end)
+			end)
+
+			describe("and the handler is called with a URL not containing a code", function()
+				before_each(function()
+					GPhotoAPIMock.URLCallback = nil
+				end)
+
+				it("shouldn't call GPhotoAPI.URLCallback or error", function()
+					assert.has_no.errors(function()
+						GPhotoURLHandler.URLHandler(url_with_code)
+					end)
+				end)
 			end)
 		end)
 	end)
